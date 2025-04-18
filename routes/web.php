0e8031967dcc5ee688eed71def6fc04c31d12513
Farid Rzayev <ferid_7003@mail.ru>
2025-04-18 16:58:02 +0400

    feat: add route protection with basic auth

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\ExcelImportController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ExcelImportController::class, 'showUploadForm']);
-Route::post('/excel/upload', [ExcelImportController::class, 'upload']);
-Route::get('/excel/progress', [ExcelImportController::class, 'progress']);
-Route::get('/rows', [ExcelImportController::class, 'showData']);
+Route::middleware('auth.basic')->group(function () {
+    Route::get('/', [ExcelImportController::class, 'showUploadForm']);
+    Route::post('/excel/upload', [ExcelImportController::class, 'upload']);
+    Route::get('/excel/progress', [ExcelImportController::class, 'progress']);
+    Route::get('/rows', [ExcelImportController::class, 'showData']);
+});
+
 
